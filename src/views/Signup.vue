b6499b3f3f2d89c46b75e6b2bc6721a45f435247
ro <kronew6@gmail.com>
2022-02-16 10:54:17 -0800

    adds frontend validations

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,7 +4,8 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      newUserParams: {},
+      // newUserParams: {},
+      newUserParams: { name: "", password: "" },
       errors: [],
     };
   },
@@ -27,6 +28,7 @@ export default {
 <template>
   <div class="signup">
     <form v-on:submit.prevent="submit()">
+      <!-- <p>newUserParams: {{ newUserParams }}</p> -->
       <h1>Signup</h1>
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
@@ -34,6 +36,9 @@ export default {
       <div>
         <label>Name:</label>
         <input type="text" v-model="newUserParams.name" />
+        <br />
+        <!-- small tags define smaller text like copyright info and comments -->
+        <small>{{ 25 - newUserParams.name.length }} characters remaining</small>
       </div>
       <br />
       <div>
@@ -44,14 +49,30 @@ export default {
       <div>
         <label>Password:</label>
         <input type="password" v-model="newUserParams.password" />
+        <br />
+        <small class="danger" v-if="newUserParams.password.length > 20">Must be less than 20 characters!</small>
       </div>
       <br />
       <div>
         <label>Password confirmation:</label>
         <input type="password" v-model="newUserParams.password_confirmation" />
       </div>
+      <small
+        class="danger"
+        v-if="newUserParams.password !== newUserParams.password_confirmation && newUserParams.password.length > 0"
+      >
+        Passwords do not match!
+      </small>
+      <br />
       <br />
       <input type="submit" value="Submit" />
     </form>
   </div>
 </template>
+
+<!-- what does scoped mean -->
+<style scoped>
+.danger {
+  color: rgb(171, 45, 45);
+}
+</style>
